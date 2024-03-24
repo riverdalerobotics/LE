e7fdd71ddd42f79036914563bfa009a224b9fa5a
Galeteanu13 <nicolas.galeteanu@gmail.com>
2024-03-23 21:17:48 -0400

    Comments and error fixing

--- LE/src/main/java/frc/robot/OperatorInput.java
+++ LE/src/main/java/frc/robot/OperatorInput.java
@@ -33,6 +33,8 @@ public class OperatorInput {
    public boolean intakePower(){
         return driverController.getBButton();
    }
+
+   //LIAM COMMENT: Method names should start lowercase
     public double ChassisSpeedThingy(){
         return driverController.getLeftY();
     }
